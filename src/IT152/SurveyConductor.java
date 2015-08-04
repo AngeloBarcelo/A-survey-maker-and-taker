5d2c1df86b21a3e038f24304e96b95d258833723
Angelo Barcelo <vergil411@hotmail.com>
2015-08-04 10:49:18 -0500

    Updated the formating of the output to make it look better. Also updated the surveyresults() to print in a grid with a little more detail for the end user.

--- src/IT152/SurveyConductor.java
+++ src/IT152/SurveyConductor.java
@@ -1,14 +1,13 @@
-/* This is just a simple survey taker. The first user enteres the number of 
- * questions and people taking the surevey. Then hands the surevey off to the 
- * peopele takign the survey. The results are provided in  grid form and allows 
- * the user to review the answers provided for a particulare question.
+/* This is just a simple survey taker. The first user enters the number of 
+ * questions and people taking the survey. Then hands the survey off to the 
+ * people taking the survey. The results are provided in grid form and allows 
+ * the user to review the answers provided for a particular question.
  */
 
 package IT152;
 
 import java.util.Scanner;
 import IT152.Survey;
-import java.util.Arrays;
 
 /*
  * @author Angelo Barcelo 
@@ -17,14 +16,15 @@ import java.util.Arrays;
  * angelobarcelo@hotmail.com
  */
 public class SurveyConductor
-    {
+{
     public static void main(String[] args)
-        {
+    {
+    
         Survey newSurvey = new Survey();
         
         //Prints welcoming message and asks if they want to add custom survey title
-        System.out.println("Hello there survey maker. Would you like to add a " +
-                         "custom title for your survey?");
+        System.out.println("Hello there! Would you like to enter a custom title " +
+                           "for the survey you are about to create?");
         
         Scanner YesNo = new Scanner(System.in);
         
@@ -34,7 +34,7 @@ public class SurveyConductor
         true and calls the overloaded Survery method and sets the new survey 
         title. If anything else is entered the default survey method is called*/
         if (customTitle.equalsIgnoreCase("y")||customTitle.equalsIgnoreCase("yes"))
-            {
+        {
             System.out.println("Sounds good! What would you like to call this " +
                                "survey?");
             
@@ -43,16 +43,17 @@ public class SurveyConductor
             String surveyTile =  newSurveyTitle.nextLine();
 
             newSurvey = new Survey(surveyTile);
-            }
+        }
         else 
-            {
+        {
             newSurvey = new Survey();
-            }
-        
+        }
+        System.out.println();
         //Displays title of survey.This confirms what method was called
         System.out.printf("Got it, going forward this survey will be " +
                               "titled: %s\n", newSurvey.getSurveyTitle());
         
+    
         //Calls the enterQuestions method
         newSurvey.enterQuestions();
         
@@ -61,48 +62,48 @@ public class SurveyConductor
          * the user */
         for (int respondentCounter =1; respondentCounter<newSurvey.numberOfRespondentsGet()+
              1; respondentCounter++ )
-            {
+        {
             for (int qustionNumb =0; qustionNumb <newSurvey.numberOfQuestionsGet();qustionNumb++ )
-                {
+            {
                 newSurvey.generateRespondentsId ();
                 
                 newSurvey.presentQuestion(qustionNumb,respondentCounter);
                 
                 //Sets variable response to 0 to prep for the validation loop
-                int response =0;
-                
-                /*validates the user response to that it is 1-5 */
-                do
-                    {
-                    System.out.println("Please enter numbered response 1-5");
-                    Scanner rating = new Scanner(System.in);
-                    response = rating.nextInt();
-                    } 
-                while (response<1||response>5);
-             
+                int response=0;
+
                 //Calls the logResponse method with what was entered and in the loop
                 newSurvey.logResponse (qustionNumb, respondentCounter, response);
-                }    
-            }
+            }    
+        }
+        
+        System.out.println();
+        
         //Calls the displaySurveyResults method
         newSurvey.displaySurveyResults();
+        
+        System.out.println();
         
         //Sets variable entered to 0 to prep for the validation loop
         int entered =0;
         
-        //Validates what is being enterd from 1 to the number of questions prev entered
-        while (entered < 1 ||entered > newSurvey.numberOfQuestionsGet())
-            {
-            System.out.println("What question would you like to review the results for?");
+        System.out.println("Would you like to see all of the answers provided to " +
+                           "one particulr question?");
         
-            Scanner displayResults = new Scanner (System.in);
-            
-            entered =  displayResults.nextInt();
-            }
+        Scanner questionReview = new Scanner(System.in);
         
-        //Calls the displayQuestionStats method
-        newSurvey.displayQuestionStats(entered);
+        String showQuestion = questionReview.next();
         
+        /* If the user enters any version of "y" or "yes" the first statment is 
+        true and calls the overloaded Survery method and sets the new survey 
+        title. If anything else is entered the default survey method is called*/
+        if (showQuestion.equalsIgnoreCase("y")||showQuestion.equalsIgnoreCase("yes"))
+        {
+            System.out.println();
+            //Calls the displayQuestionStats method
+            newSurvey.displayQuestionStats(entered);   
+        }
+        System.out.println();
         //Calls the topRatedQuestion method
         newSurvey.topRatedQuestion();
         
@@ -110,9 +111,9 @@ public class SurveyConductor
         newSurvey.lowRatedQuestion();
         
         //Prints a nice goodbye message with the survey title 
-        System.out.println (String.format("Thank you for partisipating in %s " +
+        System.out.println(String.format("\nThank you for partisipating in %s " +
                             "\nHave a nice day", newSurvey.getSurveyTitle()));                   
-        }
     }
+}
     
     
